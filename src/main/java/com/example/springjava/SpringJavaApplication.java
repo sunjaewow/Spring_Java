caf6e95feb75b498c128249be32eb2329542b7b5
sunjaewow <chltjswo789@naver.com>
2025-07-01 23:44:45 +0900

    답 없는 코드

--- src/main/java/com/example/springjava/SpringJavaApplication.java
+++ src/main/java/com/example/springjava/SpringJavaApplication.java
@@ -1,13 +1,35 @@
 package com.example.springjava;
 
+import dao.UserDao;
+import domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class SpringJavaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringJavaApplication.class, args);
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("chltjswo");
+        user.setName("최선재");
+        user.setPassword("good");
+
+        dao.add(user);
+
+        System.out.println(user.getId()+"등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId()+"조회 성공");
+
     }
 
 }
