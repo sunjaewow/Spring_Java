e60d0d80e3da67963e863cfad0069e7403a52f71
sunjaewow <chltjswo789@naver.com>
2025-07-02 00:08:25 +0900

    Separation of Concerns

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -6,8 +6,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring_java", "root", "fpdlswj365");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
         ps.setString(1, user.getId());
@@ -23,8 +22,7 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/spring_java","root","fpdlswj365");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id=?");
         ps.setString(1, id);
@@ -40,5 +38,10 @@ public class UserDao {
         ps.close();
         c.close();
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/spring_java","root","fpdlswj365");
     }
 }
