eb350ec0b079227d687a3248424aea21f2c6f136
sunjaewow <chltjswo789@naver.com>
2025-07-05 11:23:09 +0900

    초난감 예외처리

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -3,6 +3,7 @@ package dao;
 import domain.User;
 
 import javax.sql.DataSource;
+import javax.xml.transform.Result;
 import java.sql.*;
 
 public class UserDao {
@@ -47,5 +48,76 @@ public class UserDao {
         c.close();
         return user;
     }
+
+    public void deleteAll() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+
+                }
+            }
+        }
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try{
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select count(*) from users");
+
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+
+        } catch (SQLException e) {
+            throw e;
+        }finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+
+                }
+            }
+        }
+
+    }
+
 
 }
