6c345cf6c025f8f006750a8bce8bc0badb4b20a2
sunjaewow <chltjswo789@naver.com>
2025-07-07 15:47:58 +0900

    spring에서 제공하는 템플릿/콜백인 jdbcTemplate 사용

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,31 +2,72 @@ package dao;
 
 import dao.strategy.StatementStrategy;
 import domain.User;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
 
-    private final JdbcContext jdbcContext;
+    private DataSource dataSource;
 
-    public UserDao(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+
+        this.dataSource = dataSource;
     }
 
     public void add(final User user) throws SQLException {
-        jdbcContext.executeUpdateQuery("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(), user.getPassword());
+//        jdbcContext.executeUpdateQuery("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(), user.getPassword());
+        this.jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
+
     public void deleteAll()throws SQLException {//중첩 클래스, 익명 내부 클래스
-        jdbcContext.executeUpdateQuery("delete from users");
+//        jdbcContext.executeUpdateQuery("delete from users"); 스프링은 jdbc를 이용하는 dao에서 사용할 수 있도록 준비된 다양한 템플릿과 콜백을 제공
+        //할 뿐만아니라 자주 사용되는 패턴을 가진 콜백은 다시 템플릿에 결합시켜서 간단한 메소드 호출만으로 사용이 가능하도록 만들어져 있음.
+        this.jdbcTemplate.update("delete from users");//그래서 이렇게 간단하게 메서드를 이용해서 가능
     }
 
     public int getCount() throws SQLException {
-        return jdbcContext.executeQuery("select count(*)from users");
+//        return jdbcContext.executeQuery("select count(*)from users");
+//        return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//            @Override
+//            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//                return con.prepareStatement("select count(*)from users");
+//            }
+//        }, new ResultSetExtractor<Integer>() {
+//            @Override
+//            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+//                rs.next();
+//                return rs.getInt(1);
+//            }
+//        });
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+
     }
 
     public User get(String id) throws SQLException {
-        return jdbcContext.executeQuery(id,"select  * from users where id=?");
+//        return jdbcContext.executeQuery(id,"select  * from users where id=?");
+        return this.jdbcTemplate.queryForObject("select * from users where id=?",//조회 결과가 없는 경우 예외를 처리해줘야하는데
+                // queryforobject는 예외를 던지도록 만들어져 있다. 미친
+                new Object[]{id},
+                new RowMapper<User>() {
+                    @Override
+                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        User user = new User();
+                        user.setId(rs.getString("id"));
+                        user.setName(rs.getString("name"));
+                        user.setPassword(rs.getString("password"));
+                        return user;
+                    }
+                });
     }
 
 
---
+++ src/test/java/com/example/springjava/CalcSumTest.java
@@ -0,0 +1,11 @@
+package com.example.springjava;
+
+import org.junit.jupiter.api.Test;
+
+public class CalcSumTest {
+
+    @Test
+    public void sumOfNumbers() {
+    }
+
+}
