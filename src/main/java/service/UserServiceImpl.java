9bb84df59e03421c4b97ca15ca79ba4f45c8946f
sunjaewow <chltjswo789@naver.com>
2025-07-22 19:40:05 +0900

    분리된 트랜잭션 기능

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -63,31 +63,31 @@ public class UserServiceImpl implements UserService{
         }
     }
 
-    public void upgradeLevelsInternal() throws SQLException {
-//        TransactionSynchronizationManager.initSynchronization();//트랜잭션 동기화 매니저를 이용해 동기화 작업을 초기화한다.
-//        Connection c = DataSourceUtils.getConnection(dataSource); jdbc일 때
-//        c.setAutoCommit(false);//db 커넥션을 생성하고, 트랜잭션을 시작한다. 이후의 dao 작업은 모두 여기서 시작한 트랜잭션 안에서 진행된다.
-//        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);//jdbc 트랜잭션 추상 오브젝트 생성
-//        PlatformTransactionManager txManager = new JtaTransactionManager();//글로벌 트랜잭션으로 변경
-//        TransactionStatus status = txManager.getTransaction(new DefaultTransactionDefinition());//트랜잭션 추상화
-        TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
-        //각각 다른 트랜잭션을 이용하는경우
-        try {
-            upgradeLevelsInternal();
-//            c.commit();
-            this.transactionManager.commit(status);
-        } catch (Exception e) {
-//            c.rollback();
-            this.transactionManager.rollback(status);
-            throw e;
-        }
-//        finally {
-//            DataSourceUtils.releaseConnection(c, dataSource);//db connection을 안전하게 닫는다.
-//            TransactionSynchronizationManager.unbindResource(this.dataSource);
-//            TransactionSynchronizationManager.clearSynchronization();
-//            //동기화 작업 종료 및 정리
+//    public void upgradeLevelsInternal() throws SQLException {
+////        TransactionSynchronizationManager.initSynchronization();//트랜잭션 동기화 매니저를 이용해 동기화 작업을 초기화한다.
+////        Connection c = DataSourceUtils.getConnection(dataSource); jdbc일 때
+////        c.setAutoCommit(false);//db 커넥션을 생성하고, 트랜잭션을 시작한다. 이후의 dao 작업은 모두 여기서 시작한 트랜잭션 안에서 진행된다.
+////        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);//jdbc 트랜잭션 추상 오브젝트 생성
+////        PlatformTransactionManager txManager = new JtaTransactionManager();//글로벌 트랜잭션으로 변경
+////        TransactionStatus status = txManager.getTransaction(new DefaultTransactionDefinition());//트랜잭션 추상화
+//        TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+//        //각각 다른 트랜잭션을 이용하는경우
+//        try {
+//            upgradeLevelsInternal();
+////            c.commit();
+//            this.transactionManager.commit(status);
+//        } catch (Exception e) {
+////            c.rollback();
+//            this.transactionManager.rollback(status);
+//            throw e;
 //        }
-    }
+////        finally {
+////            DataSourceUtils.releaseConnection(c, dataSource);//db connection을 안전하게 닫는다.
+////            TransactionSynchronizationManager.unbindResource(this.dataSource);
+////            TransactionSynchronizationManager.clearSynchronization();
+////            //동기화 작업 종료 및 정리
+////        }
+//    }
 
     private void sendUpgradeEmail(User user) {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
